21a482a29535db942ac17e90052e7b7883e13cee
Ahmad Kaouk <ahmadkaouk.93@gmail.com>
2023-04-03 00:14:47 +0200

    boilerplate for the reputation token

---
+++ contracts/reputation_token/lib.rs
@@ -0,0 +1,26 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+#![feature(min_specialization)]
+
+#[ink::contract]
+mod reputation_token {
+    use openbrush::{
+        contracts::psp37::*,
+        traits::{Storage, String},
+    };
+
+    #[ink(storage)]
+    #[derive(Default, Storage)]
+    pub struct ReputationToken {
+        #[storage_field]
+        psp37: psp37::Data,
+    }
+
+    impl PSP37 for ReputationToken {}
+
+    impl ReputationToken {
+        #[ink(constructor)]
+        pub fn new() -> Self {
+            Self::default()
+        }
+    }
+}
